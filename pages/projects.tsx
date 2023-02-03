8d8d1b713c21d94249980744e6dc954650aa9d30
Tawnee <allprt89@gmail.com>
2023-02-02 20:48:38 -0700

    widen label spacing

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -107,8 +107,8 @@ const Projects = ({}: ProjectsProps) => {
       <Section top={false} bottom={false}>
         <Wrapper>
           <div className="flex flex-col md:flex-row justify-center">
-            <div className="basis-1/4 md:text-right pt-10 md:pt-12 lg:pt-20 pl-10 md:pl-0 md:pr-12 lg:pr-32 border-l border-gray-500 md:border-none">
-              <p className="text-lg font-semibold text-gray-500 uppercase">
+            <div className="basis-1/4 md:text-right pt-12 md:pt-12 lg:pt-20 pl-10 md:pl-0 md:pr-12 lg:pr-32 border-l border-gray-500 md:border-none">
+              <p className="text-lg font-semibold text-gray-500 uppercase tracking-wider leading-loose">
                 the motivation
               </p>
             </div>
@@ -123,7 +123,7 @@ const Projects = ({}: ProjectsProps) => {
 
           <div className="flex flex-col md:flex-row justify-center">
             <div className="basis-1/4 md:text-right pt-10 md:pt-12 lg:pt-20 pl-10 md:pl-0 md:pr-12 lg:pr-32 border-l border-gray-500 md:border-none">
-              <p className="text-lg font-semibold text-gray-500 uppercase pt-8 md:pt-0">
+              <p className="text-lg font-semibold text-gray-500 uppercase pt-8 md:pt-0 tracking-wider leading-loose">
                 the solution
               </p>
             </div>
