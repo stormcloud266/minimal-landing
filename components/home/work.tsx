5ff93343cfa906c09d81576efb4e0815c4c6ff40
Tawnee <allprt89@gmail.com>
2023-02-19 10:16:48 -0700

    update animations

--- components/home/work.tsx
+++ components/home/work.tsx
@@ -10,83 +10,84 @@ import Section from "@components/ui/section";
 import Wrapper from "@components/ui/wrapper";
 
 import { TypeCase_study } from "types/contentfulModels";
+import Fade from "@components/animations/fade";
 interface WorkProps {
   projects: TypeCase_study[];
 }
 
 const Work = ({ projects }: WorkProps) => {
   const [index, setIndex] = useState(0);
+  const project = projects[index];
 
   return (
     <Section id="work">
       <Wrapper>
-        <motion.div layout>
+        <Fade>
           <h2 className="text-center mb-28">Recent Projects</h2>
-          <div className="flex justify-center flex-wrap gap-x-8 gap-y-4">
-            {projects.map(({ fields }, i: number) => (
-              <button
-                key={fields.slug}
-                onClick={() => setIndex(i)}
-                className={clsx(
-                  "transition-colors",
-                  i === index
-                    ? "text-white"
-                    : "text-gray-500 hover:text-gray-300"
-                )}
-              >
+        </Fade>
+        <div className="flex justify-center flex-wrap gap-x-8 gap-y-4">
+          {projects.map(({ fields }, i: number) => (
+            <button
+              key={fields.slug}
+              onClick={() => setIndex(i)}
+              className={clsx(
+                "transition-colors",
+                i === index ? "text-white" : "text-gray-500 hover:text-gray-300"
+              )}
+            >
+              <Fade inline>
                 <h3 className="text-current">{fields.title}</h3>
-              </button>
-            ))}
-          </div>
-          <div className="mt-16 flex flex-col md:flex-row gap-16 lg:gap-36">
-            <div className="max-w-prose flex-1">
-              <motion.div
-                key={projects[index].fields.display_tags.list}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                className="flex flex-wrap gap-2 mb-4"
-              >
-                {projects[index].fields.display_tags.list.map(
-                  (text: string) => (
+              </Fade>
+            </button>
+          ))}
+        </div>
+        <Fade>
+          <motion.div
+            key={project.sys.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="mt-16 flex flex-col md:flex-row gap-16 lg:gap-36">
+              <div className="max-w-prose flex-1">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.fields.display_tags.list.map((text: string) => (
                     <Tag key={text + index}>{text}</Tag>
-                  )
-                )}
-              </motion.div>
+                  ))}
+                </div>
 
-              <motion.p
-                key={projects[index].fields.intro_text}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                className="text-gray-200 mb-4"
-              >
-                {projects[index].fields.intro_text}
-              </motion.p>
-              <TextLink to={`/projects/${projects[index].fields.slug}`} accent>
-                Read More about This Project
-              </TextLink>
+                <p className="text-gray-200 mb-4">
+                  {project.fields.intro_text}
+                </p>
+
+                <TextLink to={`/projects/${project.fields.slug}`} accent>
+                  Read More about This Project
+                </TextLink>
+              </div>
+              <div className="max-w-2x flex-1">
+                <h4 className="mb-6 text-xl">Highlights</h4>
+                <ul>
+                  {project.fields.challenges_list.list.map(
+                    (text: string, i: number) => (
+                      <motion.li
+                        initial={{ opacity: 0, y: 5 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: i * 0.15 }}
+                        key={i}
+                        className="mb-8 flex items-start"
+                      >
+                        <span className="mr-4 mt-2 text-gray-300 block bg-gray-700 rounded">
+                          <AngleRightIcon />
+                        </span>{" "}
+                        <span>{text}</span>
+                      </motion.li>
+                    )
+                  )}
+                </ul>
+              </div>
             </div>
-            <div className="max-w-2x flex-1">
-              <h4 className="mb-6 text-xl">Highlights</h4>
-              <ul>
-                {projects[index].fields.challenges_list.list.map(
-                  (text: string, i: number) => (
-                    <motion.li
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      key={text + i}
-                      className="mb-8 flex items-start"
-                    >
-                      <span className="mr-4 mt-2 text-gray-300 block bg-gray-700 rounded">
-                        <AngleRightIcon />
-                      </span>{" "}
-                      <span>{text}</span>
-                    </motion.li>
-                  )
-                )}
-              </ul>
-            </div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </Fade>
       </Wrapper>
     </Section>
   );
--- components/projects/process.tsx
+++ components/projects/process.tsx
@@ -1,5 +1,6 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Document } from "@contentful/rich-text-types";
+import { m as motion } from "framer-motion";
 
 import Fade from "@components/animations/fade";
 import Section from "@components/ui/section";
@@ -11,6 +12,27 @@ interface ProcessProps {
   body: Document;
   list: string[];
 }
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 5 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
 
 const Process = ({ body, list }: ProcessProps) => {
   return (
@@ -27,20 +49,24 @@ const Process = ({ body, list }: ProcessProps) => {
               </div>
             </Fade>
 
-            <ul className="max-w-prose">
-              {list.map((text) => (
-                <li key={text}>
-                  <Fade inline>
-                    <span className="text-gray-200 mt-6 flex items-start">
-                      <span className="mr-4 mt-1 text-gray-300 block bg-gray-700 rounded">
-                        <AngleRightIcon />
-                      </span>
-                      {text}
+            <motion.ul
+              className="max-w-prose"
+              variants={containerVariants}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+            >
+              {list.map((text, i) => (
+                <motion.li variants={itemVariants} key={text}>
+                  <span className="text-gray-200 mt-6 flex items-start">
+                    <span className="mr-4 mt-1 text-gray-300 block bg-gray-700 rounded">
+                      <AngleRightIcon />
                     </span>
-                  </Fade>
-                </li>
+                    {text}
+                  </span>
+                </motion.li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
         </div>
       </Wrapper>
